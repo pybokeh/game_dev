2799b4acfa8b49756614cf8837443f9aa9e38563
pybokeh <pybokeh@protonmail.com>
2020-07-05 16:13:20 -0400

    first commit

---
+++ alpha_fighter_game.py
@@ -0,0 +1,155 @@
+"""
+Better Move Sprite With Keyboard
+
+Simple program to show moving a sprite with the keyboard.
+This is slightly better than sprite_move_keyboard.py example
+in how it works, but also slightly more complex.
+
+Artwork from http://kenney.nl
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.sprite_move_keyboard_better
+"""
+
+import arcade
+import os
+
+ALPHA_VERITECH_SCALING = 0.2
+
+SCREEN_WIDTH = 1450
+SCREEN_HEIGHT = 840
+SCREEN_TITLE = "RoboTech Alpha Fighter Game"
+
+MOVEMENT_SPEED = 7
+
+
+class Alpha_Fighter(arcade.Sprite):
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self, width, height, title):
+        """
+        Initializer
+        """
+
+        # Call the parent class initializer
+        super().__init__(width, height, title)
+
+        self.background = None
+
+        # Variables that will hold sprite lists
+        self.player_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+
+        # Track the current state of what key is pressed
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+
+
+    def setup(self):
+        """ Set up the game and initialize the variables. """
+
+        self.background = arcade.load_texture("images/space.jpg")
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+
+        # Set up the player
+        self.player_sprite = Alpha_Fighter("images/alpha_veritech_blue_right.png", ALPHA_VERITECH_SCALING)
+        self.player_sprite.center_x = 100
+        self.player_sprite.center_y = 100
+        self.player_list.append(self.player_sprite)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
+        arcade.start_render()
+
+        # Draw the background texture
+        arcade.draw_lrwh_rectangle_textured(0, 0,
+                                            SCREEN_WIDTH, SCREEN_HEIGHT,
+                                            self.background)
+
+        # Draw all the sprites.
+        self.player_list.draw()
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Calculate speed based on the keys pressed
+        self.player_sprite.change_x = 0
+        self.player_sprite.change_y = 0
+
+        if self.up_pressed and not self.down_pressed:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif self.down_pressed and not self.up_pressed:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        if self.left_pressed and not self.right_pressed:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif self.right_pressed and not self.left_pressed:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+        # Call update to move the sprite
+        # If using a physics engine, call update on it instead of the sprite
+        # list.
+        self.player_list.update()
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP:
+            self.up_pressed = True
+        elif key == arcade.key.DOWN:
+            self.down_pressed = True
+        elif key == arcade.key.LEFT:
+            self.left_pressed = True
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = True
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP:
+            self.up_pressed = False
+        elif key == arcade.key.DOWN:
+            self.down_pressed = False
+        elif key == arcade.key.LEFT:
+            self.left_pressed = False
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = False
+
+
+def main():
+    """ Main method """
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
